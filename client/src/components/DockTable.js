4494786a1152199a430ad1bf6e8f7d63d35b1865
Derek Houck <derek.houck@modernmsg.com>
2020-12-21 17:36:05 -0800

    Display Dock names

---
+++ client/src/components/DockTable.js
@@ -0,0 +1,24 @@
+import React, {useEffect, useState} from 'react';
+
+const DockTable = () => {
+  const [docks, setDocks] = useState([])
+
+  useEffect(() => {
+    if (docks.length === 0) 
+      fetch('/docks')
+        .then( res => res.json())
+        .then(data => setDocks(data));
+  }, [docks])
+
+  return (
+    <div className="dock-table">
+      {docks.map(d => (
+        <div>
+          <h2>{d.name}</h2>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default DockTable
