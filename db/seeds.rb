52989710ea5e197a87eadfa544328121094d2ba2
Derek Houck <derek.houck@modernmsg.com>
2020-12-24 16:47:15 -0800

    Add comments to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
 
+# Create 20 docks
 20.times do |i|
   name = "Dock #{i + 1}"
   Dock.create(name: name)
@@ -14,8 +9,11 @@ end
 boat_names = ['Boaty McBoatface', 'Titanic', 'Shorty', 'Aquaboat', 'Carbon Dater']
 colors = %w[red yellow blue black green]
 
+# Create five boats with random lengths
 5.times do |i|
   boat = Boat.create(name: boat_names[i], length: rand(5..60), color: colors[i])
+  # Randomly assign boats over a certain length to a dock
+  # Not guaranteed that any boats will be docked by default, but it's likely and more fun than statically assigning them
   if boat.length > 30
     boat.dock_id = rand(20)
     boat.save
